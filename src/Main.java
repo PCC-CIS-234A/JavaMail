d16c585be6026c46cbcf862a0c2eb2fceade111e
Marc Goodman <marc.goodman@pcc.edu>
2019-05-16 00:54:11 -0700

    Send mail with JavaMail.jar

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        final String username = "CIS234A@gmail.com";
+        final String password = "CIS234A_mail";
+
+        Properties props = new Properties();
+
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from-email@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("marc.goodman@pcc.edu"));
+            message.setSubject("Testing Subject");
+            message.setDataHandler(new DataHandler("<p>This is a <b>bold</b> test of formatted messages.</p>", "text/html"));
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (javax.mail.MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
